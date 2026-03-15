5b9a8d29094da904845884d52ccdfae393fd5b1d
benja <benjamin.lopezesteban@gmail.com>
2026-03-15 18:16:09 -0300

    primera version proyecto

---
+++ demo/src/test/java/ejemplo/trabajo/demo/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package ejemplo.trabajo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
